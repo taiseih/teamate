acd40ed080900b898590db4da9c4cbadcaf21ada
taiseih <taisei.hayashizaki@gmail.com>
2023-03-10 09:59:52 +0900

    edit, Update

--- app/Http/Controllers/Admin/Members/MembersController.php
+++ app/Http/Controllers/Admin/Members/MembersController.php
@@ -67,7 +67,9 @@ class MembersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $users = User::findOrFail($id);
+
+        return view('admin.members.edit', compact('users'));
     }
 
     /**
@@ -79,7 +81,15 @@ class MembersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $users = User::findOrFail($id);
+        $users->name = $request->name;
+        $users->email = $request->email;
+        $users->job = $request->job;
+        $users->password = Hash::make($request->password);
+        $users->save();
+
+        return redirect()->route('admin.members.index');
+
     }
 
     /**
