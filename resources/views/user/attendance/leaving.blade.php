33c7a756fbe70025972d54ab5b9ee8f508a3efc4
taiseih <taisei.hayashizaki@gmail.com>
2023-04-14 12:31:27 +0900

    https通信用

--- resources/views/user/attendance/leaving.blade.php
+++ resources/views/user/attendance/leaving.blade.php
@@ -11,8 +11,10 @@
     <!-- Fonts -->
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
-    <script src="{{ asset('js/app.js') }}" defer></script>
+    {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <script src="{{ asset('js/app.js') }}" defer></script> --}}
+        <link rel="stylesheet" href="https://www.mng-teamate.com/css/app.css">
+    <script src="https://www.mng-teamate.com/js/app.js" defer></script>
 </head>
 
 <body class="font-sans antialiased">
