8728e68687397059d931944701df906ef75e402b
taiseih <taisei.hayashizaki@gmail.com>
2023-04-19 19:36:05 +0900

    taskの各種名前を業務に変更

--- resources/views/user/task/create.blade.php
+++ resources/views/user/task/create.blade.php
@@ -9,17 +9,17 @@
                         <div class="container px-5 py-24 mx-auto flex">
                             <div
                                 class="lg:w-1/3 md:w-1/2 bg-white rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md mx-auto">
-                                <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">タスク登録</h2>
-                                <p class="leading-relaxed mb-5 text-gray-600">本日のタスクを登録してください</p>
+                                <h2 class="text-gray-900 text-lg mb-1 font-medium title-font">業務登録</h2>
+                                <p class="leading-relaxed mb-5 text-gray-600">本日の業務を登録してください</p>
                                 <form method="POST" action="{{ route('user.task.store') }}">
                                     @csrf
                                     <div class="relative mb-4">
-                                        <label for="title" class="leading-7 text-sm text-gray-600">タスク名</label>
+                                        <label for="title" class="leading-7 text-sm text-gray-600">業務名</label>
                                         <input type="title" id="title" name="title" required
                                             class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out">
                                     </div>
                                     <div class="relative mb-4">
-                                        <label for="information" class="leading-7 text-sm text-gray-600">タスク詳細</label>
+                                        <label for="information" class="leading-7 text-sm text-gray-600">業務詳細</label>
                                         <textarea id="information" name="information"
                                             class="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                                     </div>
