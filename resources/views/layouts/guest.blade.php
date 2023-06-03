583db9e1fbd25821d0ae52db1905690d204611c6
taiseih <taisei.hayashizaki@gmail.com>
2023-06-03 14:35:05 +0900

    ゲストレイアウトのCSS変更

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,8 +15,8 @@
 
         {{-- <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <script src="{{ asset('js/app.js') }}" defer></script> --}}
-    <link rel="stylesheet" href="https://www.mng-teamate.com/css/app.css">
-    <script src="https://www.mng-teamate.com/js/app.js" defer></script>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <script src="{{asset('js/app.js')}}" defer></script>
         
     </head>
     <body>
